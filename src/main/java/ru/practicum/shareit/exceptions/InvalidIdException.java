92df92156d06885669b7029163424baebc23bc91
Scoobe <semenskakodub@yandex.ru>
2024-04-01 23:30:19 +0300

    13ый проект

--- src/main/java/ru/practicum/shareit/exceptions/InvalidIdException.java
+++ src/main/java/ru/practicum/shareit/exceptions/InvalidIdException.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class InvalidIdException extends RuntimeException{
+public class InvalidIdException extends RuntimeException {
     private final HttpStatus httpStatus;
 
     public InvalidIdException(String message, HttpStatus httpStatus) {
